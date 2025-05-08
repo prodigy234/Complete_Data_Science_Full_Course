6b4f6bd911544718ebb76b6a8682457e1df8a564
prodigy234 <k.gbenga234@gmail.com>
2025-05-08 14:45:44 +0100

    tenth commit

---
+++ Databases/Prodigy_holdings/Regex database.sql
@@ -0,0 +1,30 @@
+-- CREATE DATABASE regex_database
+
+-- USE regex_database
+
+-- CREATE TABLE users (
+--     id INT PRIMARY KEY,
+--     name VARCHAR(50)
+-- );
+
+-- INSERT INTO users (id, name) VALUES
+-- (1, 'Alice'),
+-- (2, 'Alan'),
+-- (3, 'Amanda'),
+-- (4, 'Alec'),
+-- (5, 'Aaron'),
+-- (6, 'Bob'),
+-- (7, 'Bobby'),
+-- (8, 'Boob'),
+-- (9, 'Carla'),
+-- (10, 'Diana'),
+-- (11, 'Ella'),
+-- (12, 'john23'),
+-- (13, 'Alex007'),
+-- (14, 'Max'),
+-- (15, 'Sam'),
+-- (16, 'Anna'),
+-- (17, 'Otto'),
+-- (18, 'Emmanuel'),
+-- (19, 'Manfred'),
+-- (20, 'user1');
